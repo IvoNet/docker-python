36f4c298c7b7af504d7ef06170ecf67871d93ed2
Ivo Woltring <Ivo@IvoNet.nl>
2018-06-20 16:44:21 +0200

    removed the need for a jupyter password completely as this is only for local usage

--- jupyter.template.sh
+++ jupyter.template.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
 PORT=8888
-docker run --rm --name jupyter -p ${PORT}:${PORT} -v "$(pwd):/project" ivonet/py3 jupyter notebook --port=${PORT} --ip=0.0.0.0 --allow-root
+NAME=jupyter
+RUNNING=$(docker inspect --format="{{ .State.Running }}" jupyter 2> /dev/null)
+
+if [ $? -eq 1 ] || [ "$RUNNING" == "false" ]; then
+  /usr/bin/osascript -e 'display notification "You might need to refresh the browser..." with title "Jupyter" subtitle "Starting..."'
+  docker run --rm -d --name jupyter -p ${PORT}:${PORT} -v "$(pwd):/project" ivonet/py3 jupyter notebook --port=${PORT} --ip=0.0.0.0 --allow-root --no-browser
+  open http://localhost:${PORT}
+else
+  /usr/bin/osascript -e 'display notification "Stopping..." with title "Jupyter"'
+  docker stop jupyter
+  /usr/bin/osascript -e 'display notification "Stopped successfully." with title "Jupyter"'
+fi
